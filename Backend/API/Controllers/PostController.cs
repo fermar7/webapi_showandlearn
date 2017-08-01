6708b62d079df6e7194ec6af5a91f30a13a75844
fermar7 <fermar0715@gmail.com>
2017-08-01 22:15:53 +0200

    Added entity and sorting

--- Backend/API/Controllers/PostController.cs
+++ Backend/API/Controllers/PostController.cs
@@ -1,4 +1,5 @@
-﻿using API.Models;
+﻿using API.Entities;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,18 +12,27 @@ namespace API.Controllers
     public class PostController : Controller
     {
 
-        private static List<PostModel> Posts { get; } = new List<PostModel>();
+        private static List<Post> Posts { get; } = new List<Post>();
 
         [HttpGet]
         public IActionResult List() {
-            return Ok(Posts.ToList());
+            return Ok(Posts
+                .OrderByDescending(x => x.PostedAt)
+                .Select(x => new PostModel {
+                    Id = x.Id,
+                    PostedAt = x.PostedAt.ToString("dd.MM.yyyy, HH:mm"),
+                    Username = x.Username,
+                    UserTag = x.UserTag,
+                    Text = x.Text
+                })
+                .ToList());
         }
 
         [HttpPost]
         public IActionResult Post([FromBody] PostCreateModel model) {
-            var newPost = new PostModel();
+            var newPost = new Post();
             newPost.Id = Guid.NewGuid();
-            newPost.PostedAt = DateTime.Now.ToString("dd.MM.yyyy, HH:mm");
+            newPost.PostedAt = DateTime.Now;
             newPost.Username = model.Username;
             newPost.UserTag = model.UserTag;
             newPost.Text = model.Text;
---
+++ Backend/API/Entities/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string UserTag { get; set; }
+
+        public DateTime PostedAt { get; set; }
+
+        public string Text { get; set; }
+    }
+}
